bd8072e1dab06479898f52794a54ba9509b7a2ce
sahi-massi <sahimasssi@gmail.com>
2021-05-27 11:30:33 +0100

    Global Changes

--- ajax/global.js
+++ ajax/global.js
@@ -1,6 +1,13 @@
 $(function(){
 	'use strict';
 
+	/***************************************************************** 
+    **************** get loved and buyed products ********************
+    *****************************************************************/
+    $(window).on('load', function(){
+    	$("#content").load("includs/sections/home.php");
+    });
+
     /* global redirect function */
     $('.nav-link').click(function(){
         $(this).addClass("active").siblings().removeClass("active");
@@ -8,5 +15,7 @@ $(function(){
             source = "includs/sections/" + target + ".php";
         $("#content").load(source);
     });
+
+
 
 });
